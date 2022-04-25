ebdc84140b7c0ccd55b0a2215884210e1ea57992
recurshawn <shawnpereira1999@gmail.com>
2022-04-25 12:38:05 +0530

    Added ships with lighting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+//Requirements
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+
+//Styling
+import "./App.css";
+
+//Components
+import Ship from "./components/Ship";
+
+//Models
+import LuxuryShip from "./models/LuxuryShip";
+import MedicalFrigate from "./models/MedicalFrigate";
+import CargoFreighter from "./models/CargoFreighter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas
+        colorManagement
+        orthographic
+        camera={{ position: [-20, 30, -20], zoom: 30 }}
+      >
+        <Suspense fallback={null}>
+          {/* Lighting */}
+          <ambientLight intensity={0.4} />
+          <pointLight intensity={0.5} position={[-10, 0, 10]} />
+          <pointLight intensity={1.5} position={[0, -10, 0]} />
+
+          {/* Ships */}
+          <Ship position={[-15, -3, 10]} name="Luxury Ship">
+            <LuxuryShip />
+          </Ship>
+          <Ship position={[-5, -3, 0]} name="Medical Frigate">
+            <MedicalFrigate />
+          </Ship>
+          <Ship position={[13, -3, 30]} name="Cargo Freighter">
+            <CargoFreighter />
+          </Ship>
+        </Suspense>
+      </Canvas>
+    </>
   );
 }
 
---
+++ src/components/Ship.js
@@ -0,0 +1,27 @@
+import React, { useRef, useState } from "react";
+import { useSpring, a } from "@react-spring/three";
+
+//Generic Ship mesh wrapper
+ export default function Ship({ position, args, color, name, rotation, children }) {
+    const mesh = useRef(null);
+    //Basic expand state
+    const [expand, setExpand] = useState(false);
+    // React spring expand animation
+    const props = useSpring({
+      scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
+    });
+  
+    //useFrame(() => (mesh.current.rotation.y += 0.01));
+    return (
+      <a.mesh
+        position={position}
+        ref={mesh}
+        onPointerOver={() => setExpand(true)}
+        onPointerOut={() => setExpand(false)}
+        onClick={() => console.log(name, mesh)}
+        scale={props.scale}
+      >
+        {children}
+      </a.mesh>
+    );
+  };
